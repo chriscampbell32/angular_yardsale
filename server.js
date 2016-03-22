a579cc99351182ec538de7878b65e1e0f0540a21
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-22 12:53:09 -0400

    users successfully adding to db

--- server.js
+++ server.js
@@ -23,6 +23,27 @@ app.post('/user', function(req, res) {
     req.body.username = req.body.username.toLowerCase();
     User.findOne({
         'username': req.body.username
+    }).exec(function(err, user){
+        if(err){
+            console.log('error');
+            res.send(err);
+        }else{
+            if(user === null){
+                console.log(req.body);
+                var newUser = new User(req.body);
+                newUser.save(function(err, newUser){
+                    if(err){
+                        console.log(err);
+                        res.send(err);
+                    }else{
+                        res.send(newUser);
+                    }
+                });
+            } else{
+                console.log(user);
+                res.send(user);
+            }
+        }
     });
 });
 
