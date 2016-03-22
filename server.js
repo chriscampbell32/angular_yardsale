fa39e8be80218cdb1f494bec96c1b0ff720e3f0c
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-21 20:15:50 -0400

    reorganized structure, locla hosting

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ var PORT = process.env.PORT || 8080;
 var db = 'mongodb://localhost/yardsale';
 mongoose.connect(db);
 
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+    res.send(index.html);
+});
 
 app.listen(PORT, function(req, res) {
-    console.log("listening on port %s:", PORT);
+    console.log("listening on port: %s", PORT);
     
 });
