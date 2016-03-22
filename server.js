a4169df69a49044c77629f3ad785d8979fd13db4
chriscampbell32 <chriscampbell32@yahoo.com>
2016-03-22 12:47:48 -0400

    user model connecting to database

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 var db = 'mongodb://localhost/yardsale';
 mongoose.connect(db);
 
-var User = require('./models/Users');
+var User = require('./models/User');
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + "/public"));
@@ -19,9 +19,9 @@ app.get('/', function(req, res){
     res.send(index.html);
 });
 
-app.post('/user', function(req, res){
+app.post('/user', function(req, res) {
     req.body.username = req.body.username.toLowerCase();
-    Users.findOne({
+    User.findOne({
         'username': req.body.username
     });
 });
